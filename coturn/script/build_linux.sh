c552320097da787316c7b88404727e476bbe5dc9
gongluck <gongluck93@gmail.com>
2022-02-18 18:21:53 +0800

    coturn cmake compile

--- coturn/script/build_linux.sh
+++ coturn/script/build_linux.sh
@@ -12,9 +12,13 @@ sudo dos2unix ./configure
 
 #配置
 ./configure --prefix=../build
+#cmake
+#cmake -G "Unix Makefiles" -S . -B ./build -DCMAKE_BUILD_TYPE=release
 
 #编译
 sudo make -j 8
 sudo make install
+#cmake
+cmake --build ./build --clean-first --config release --target all
 
 #turnserver --min-port 40000 --max-port 60000 -L 0.0.0.0 -a -u gongluck:123456 -v -f -r nort.gov
