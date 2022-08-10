dac1284e78b8d9c27c056864d3520afc8d00225d
gongluck <1039994845@qq.com>
2022-08-10 11:18:07 +0800

    android openssl

--- openssl/script/build_android.sh
+++ openssl/script/build_android.sh
@@ -3,30 +3,34 @@
 export ANDROID_NDK_ROOT=/mnt/e/code/android/ndk/android-ndk-r21d-linux
 PATH=$ANDROID_NDK_ROOT/toolchains/llvm/prebuilt/linux-x86_64/bin:$PATH
 
-ABI=22
+abi=22
 
 cd ../src
 
 # android-arm
 make clean
-./Configure android-arm -D__ANDROID_API__=$ABI --prefix=$PWD/../install/android-arm
+platform=android-arm
+./Configure $platform -D__ANDROID_API__=$abi --prefix=$PWD/../install/$platform
 make -j 8
 make install
 
 # android-arm64
 make clean
-./Configure android-arm64 -D__ANDROID_API__=$ABI --prefix=$PWD/../install/android-arm64
+platform=android-arm64
+./Configure $platform -D__ANDROID_API__=$abi --prefix=$PWD/../install/$platform
 make -j 8
 make install
 
 # android-x86
 make clean
-./Configure android-x86 -D__ANDROID_API__=$ABI --prefix=$PWD/../install/android-x86
+platform=android-x86
+./Configure $platform -D__ANDROID_API__=$abi --prefix=$PWD/../install/$platform
 make -j 8
 make install
 
 # android-x86_64
 make clean
-./Configure android-x86_64 -D__ANDROID_API__=$ABI --prefix=$PWD/../install/android-x86_64
+platform=android-x86_64
+./Configure $platform -D__ANDROID_API__=$abi --prefix=$PWD/../install/$platform
 make -j 8
 make install
