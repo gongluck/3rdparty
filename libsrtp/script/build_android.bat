5bc8178a69e0d9cc60193762c678d9c70f2863a1
gongluck <1039994845@qq.com>
2022-08-11 17:40:31 +0800

    update libsrtp

--- libsrtp/script/build_android.bat
+++ libsrtp/script/build_android.bat
@@ -5,61 +5,47 @@ rem 需要依赖openssl
 
 set ANDROID_NDK_HOME=E:/code/android/ndk/android-ndk-r21b
 set platform=android-22
+set pathprefix=../build/android
+set buildparams=-DCMAKE_TOOLCHAIN_FILE="%ANDROID_NDK_HOME%/build/cmake/android.toolchain.cmake" -DCMAKE_MAKE_PROGRAM="%ANDROID_NDK_HOME%/prebuilt/windows-x86_64/bin/make.exe" -DANDROID_NDK="%ANDROID_NDK_HOME%"
 
 rem armeabi-v7a
 set abi=armeabi-v7a
-cmake -G "Unix Makefiles" -S ../src -B ../build/android/%abi% ^
--DCMAKE_TOOLCHAIN_FILE="%ANDROID_NDK_HOME%/build/cmake/android.toolchain.cmake" ^
--DCMAKE_MAKE_PROGRAM="%ANDROID_NDK_HOME%/prebuilt/windows-x86_64/bin/make.exe" ^
--DANDROID_NDK="%ANDROID_NDK_HOME%" ^
+cmake -G "Unix Makefiles" -S ../src -B %pathprefix%/%abi% %buildparams% ^
 -DCMAKE_BUILD_TYPE=release -DANDROID_ABI=%abi% -DANDROID_PLATFORM=%platform% ^
 -DENABLE_OPENSSL=ON -DOPENSSL_INCLUDE_DIR=../../openssl/include/android/armeabi ^
 -DOPENSSL_CRYPTO_LIBRARY=../../openssl/lib/android/armeabi/libcrypto.a ^
--DBUILD_SHARED_LIBS=OFF ^
--DBUILD_WITH_WARNINGS=OFF
-cmake --build ../build/android/%abi% --clean-first --config release --target all -- -j8
-cmake --install ../build/android/%abi% --config release --prefix ../install/android/%abi%
+-DBUILD_SHARED_LIBS=OFF -DBUILD_WITH_WARNINGS=OFF
+cmake --build %pathprefix%/%abi% --clean-first --config release --target all -- -j8
+cmake --install %pathprefix%/%abi% --config release --prefix %pathprefix%/../../install/android/%abi%
 
 rem arm64-v8a
 set abi=arm64-v8a
-cmake -G "Unix Makefiles" -S ../src -B ../build/android/%abi% ^
--DCMAKE_TOOLCHAIN_FILE="%ANDROID_NDK_HOME%/build/cmake/android.toolchain.cmake" ^
--DCMAKE_MAKE_PROGRAM="%ANDROID_NDK_HOME%/prebuilt/windows-x86_64/bin/make.exe" ^
--DANDROID_NDK="%ANDROID_NDK_HOME%" ^
+cmake -G "Unix Makefiles" -S ../src -B %pathprefix%/%abi% %buildparams% ^
 -DCMAKE_BUILD_TYPE=release -DANDROID_ABI=%abi% -DANDROID_PLATFORM=%platform% ^
 -DENABLE_OPENSSL=ON -DOPENSSL_INCLUDE_DIR=../../openssl/include/android/%abi% ^
 -DOPENSSL_CRYPTO_LIBRARY=../../openssl/lib/android/%abi%/libcrypto.a ^
--DBUILD_SHARED_LIBS=OFF ^
--DBUILD_WITH_WARNINGS=OFF
-cmake --build ../build/android/%abi% --clean-first --config release --target all -- -j8
-cmake --install ../build/android/%abi% --config release --prefix ../install/android/%abi%
+-DBUILD_SHARED_LIBS=OFF -DBUILD_WITH_WARNINGS=OFF
+cmake --build %pathprefix%/%abi% --clean-first --config release --target all -- -j8
+cmake --install %pathprefix%/%abi% --config release --prefix %pathprefix%/../../install/android/%abi%
 
 rem x86
 set abi=x86
-cmake -G "Unix Makefiles" -S ../src -B ../build/android/%abi% ^
--DCMAKE_TOOLCHAIN_FILE="%ANDROID_NDK_HOME%/build/cmake/android.toolchain.cmake" ^
--DCMAKE_MAKE_PROGRAM="%ANDROID_NDK_HOME%/prebuilt/windows-x86_64/bin/make.exe" ^
--DANDROID_NDK="%ANDROID_NDK_HOME%" ^
+cmake -G "Unix Makefiles" -S ../src -B %pathprefix%/%abi% %buildparams% ^
 -DCMAKE_BUILD_TYPE=release -DANDROID_ABI=%abi% -DANDROID_PLATFORM=%platform% ^
 -DENABLE_OPENSSL=ON -DOPENSSL_INCLUDE_DIR=../../openssl/include/android/%abi% ^
 -DOPENSSL_CRYPTO_LIBRARY=../../openssl/lib/android/%abi%/libcrypto.a ^
--DBUILD_SHARED_LIBS=OFF ^
--DBUILD_WITH_WARNINGS=OFF
-cmake --build ../build/android/%abi% --clean-first --config release --target all -- -j8
-cmake --install ../build/android/%abi% --config release --prefix ../install/android/%abi%
+-DBUILD_SHARED_LIBS=OFF -DBUILD_WITH_WARNINGS=OFF
+cmake --build %pathprefix%/%abi% --clean-first --config release --target all -- -j8
+cmake --install %pathprefix%/%abi% --config release --prefix %pathprefix%/../../install/android/%abi%
 
 rem x86_64
 set abi=x86_64
-cmake -G "Unix Makefiles" -S ../src -B ../build/android/%abi% ^
--DCMAKE_TOOLCHAIN_FILE="%ANDROID_NDK_HOME%/build/cmake/android.toolchain.cmake" ^
--DCMAKE_MAKE_PROGRAM="%ANDROID_NDK_HOME%/prebuilt/windows-x86_64/bin/make.exe" ^
--DANDROID_NDK="%ANDROID_NDK_HOME%" ^
+cmake -G "Unix Makefiles" -S ../src -B %pathprefix%/%abi% %buildparams% ^
 -DCMAKE_BUILD_TYPE=release -DANDROID_ABI=%abi% -DANDROID_PLATFORM=%platform% ^
 -DENABLE_OPENSSL=ON -DOPENSSL_INCLUDE_DIR=../../openssl/include/android/%abi% ^
 -DOPENSSL_CRYPTO_LIBRARY=../../openssl/lib/android/%abi%/libcrypto.a ^
--DBUILD_SHARED_LIBS=OFF ^
--DBUILD_WITH_WARNINGS=OFF
-cmake --build ../build/android/%abi% --clean-first --config release --target all -- -j8
-cmake --install ../build/android/%abi% --config release --prefix ../install/android/%abi%
+-DBUILD_SHARED_LIBS=OFF -DBUILD_WITH_WARNINGS=OFF
+cmake --build %pathprefix%/%abi% --clean-first --config release --target all -- -j8
+cmake --install %pathprefix%/%abi% --config release --prefix %pathprefix%/../../install/android/%abi%
 
 pause
