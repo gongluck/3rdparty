bca3d10955290dcb2703a667c415930c8d902ced
gongluck <1039994845@qq.com>
2022-10-18 14:15:05 +0800

    use release mode to build zlmediakit windows version

--- ZLMediaKit/script/build_windows.bat
+++ ZLMediaKit/script/build_windows.bat
@@ -16,6 +16,7 @@ rem ➕设置srtp库路径
 set pathprefix=../build/windows
 
 rem win32
+set buildtype=Release
 set platform=win32
 set buildpath=%pathprefix%/%platform%
 set opensslparams=-DENABLE_OPENSSL=ON -DOPENSSL_ROOT_DIR=C:/OpenSSL-Win32
@@ -23,12 +24,13 @@ set srtparams=-DSRTP_INCLUDE_DIRS=../../libsrtp/include -DSRTP_LIBRARIES=../../l
 set sctparams=-DSCTP_INCLUDE_DIRS=../../usrsctp/include -DSCTP_LIBRARIES=../../usrsctp/lib/windows/win32/usrsctp.lib
 cmake -S ../src -B %buildpath% -G "Visual Studio 16 2019" -A %platform% -DCMAKE_INSTALL_PREFIX=%pathprefix%/../../install/windows/%platform% ^
 %opensslparams% %srtparams% %sctparams% -DENABLE_WEBRTC=ON -DENABLE_CXX_API=ON -DENABLE_API_STATIC_LIB=OFF ^
--DCMAKE_CXX_FLAGS_RELEASE="/MP /MT"
+-DCMAKE_CXX_FLAGS_RELEASE="/MP /MT" -DCMAKE_BUILD_TYPE=%buildtype%
 cmake --build %buildpath% --clean-first --config release --target ALL_BUILD
 cmake --install %buildpath% --prefix %pathprefix%/../../install/windows/%platform% --config release
-xcopy /S /Y /I ..\src\release\windows\Debug\Release\* ..\install\windows\%platform%\lib\
+xcopy /S /Y /I ..\src\release\windows\%buildtype%\Release\* ..\install\windows\%platform%\lib\
 
 rem x64
+set buildtype=Release
 set platform=x64
 set buildpath=%pathprefix%/%platform%
 set opensslparams=-DENABLE_OPENSSL=ON -DOPENSSL_ROOT_DIR=C:/OpenSSL-Win64
@@ -36,9 +38,9 @@ set srtparams=-DSRTP_INCLUDE_DIRS=../../libsrtp/include -DSRTP_LIBRARIES=../../l
 set sctparams=-DSCTP_INCLUDE_DIRS=../../usrsctp/include -DSCTP_LIBRARIES=../../usrsctp/lib/windows/x64/usrsctp.lib
 cmake -S ../src -B %buildpath% -G "Visual Studio 16 2019" -A %platform% -DCMAKE_INSTALL_PREFIX=%pathprefix%/../../install/windows/%platform% ^
 %opensslparams% %srtparams% %sctparams% -DENABLE_WEBRTC=ON -DENABLE_CXX_API=ON -DENABLE_API_STATIC_LIB=OFF ^
--DCMAKE_CXX_FLAGS_RELEASE="/MP /MT"
+-DCMAKE_CXX_FLAGS_RELEASE="/MP /MT" -DCMAKE_BUILD_TYPE=%buildtype%
 cmake --build %buildpath% --clean-first --config release --target ALL_BUILD
 cmake --install %buildpath% --prefix %pathprefix%/../../install/windows/%platform% --config release
-xcopy /S /Y /I ..\src\release\windows\Debug\Release\* ..\install\windows\%platform%\lib\
+xcopy /S /Y /I ..\src\release\windows\%buildtype%\Release\* ..\install\windows\%platform%\lib\
 
 pause
