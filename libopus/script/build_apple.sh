05dfe43a5fa4ef1156b80381f1d1c3d89a84315e
gongluck <gongluck93@gmail.com>
2023-02-22 15:54:48 +0800

    update apple libopus

--- libopus/script/build_apple.sh
+++ libopus/script/build_apple.sh
@@ -3,8 +3,17 @@ rm -r ../install/apple/*
 mkdir ../build/apple
 mkdir ../install/apple
 
+# params: type(OSX_shared) build_shared_libs(ON)
+function build_OSX {
+  cmake -S ../src -B ../build/apple/$1 -G Xcode \
+    -DCMAKE_INSTALL_PREFIX=`pwd`/../install/apple/$1 \
+    -DBUILD_SHARED_LIBS=$2
+  cmake --build ../build/apple/$1 --clean-first --target ALL_BUILD --config Release
+  cmake --install ../build/apple/$1 --config Release --prefix `pwd`/../install/apple/$1
+}
+
 # params: type(iOS_shared) system(iOS) arch("armv7;arm64;i386;x86_64") build_shared_libs(ON)
-function build {
+function build_iOS {
   cmake -S ../src -B ../build/apple/$1 -G Xcode \
     -DCMAKE_SYSTEM_NAME=$2 \
     -DCMAKE_OSX_ARCHITECTURES=$3 \
@@ -18,5 +27,7 @@ function build {
   cmake --install ../build/apple/$1 --config Release --prefix `pwd`/../install/apple/$1
 }
 
-build iOS_shared iOS "armv7;arm64;i386;x86_64" ON
-build iOS_static iOS "armv7;arm64;i386;x86_64" OFF
+build_OSX OSX_shared ON
+build_OSX OSX_static OFF
+# build iOS_shared iOS "armv7;arm64;i386;x86_64" ON
+# build iOS_static iOS "armv7;arm64;i386;x86_64" OFF
