953522808627ecdf66d2f4e375add70fea196a0a
gongluck <gongluck93@gmail.com>
2024-05-29 12:38:03 +0800

    Update build_android.sh

--- webrtc/script/build_android.sh
+++ webrtc/script/build_android.sh
@@ -16,18 +16,22 @@ export PATH="$PATH:$(pwd)/depot_tools"
 mkdir webrtc
 cd webrtc
 fetch --nohooks webrtc_android
-gclient sync
+gclient sync --no-history
+#拉取特定分支
+gclient sync -r 729f79c176c8b4c3a8c1b60f39df5fb43323b27b --no-history
 
 #下载依赖
 ./src/build/install-build-deps.sh
-./src/build/install-build-deps-android.sh
+#./src/build/install-build-deps-android.sh
 
 cd src
 source build/android/envsetup.sh
+
 #native编译
 gn clean out/native_arm
 gn gen out/native_arm --args='target_os="android" target_cpu="arm"'
 ninja -C out/native_arm
+
 #打包aar
-tools_webrtc/android/build_aar.py --build-dir out/build_aar --arch "armeabi-v7a" "arm64-v8a" "x86" "x86_64"
+tools_webrtc/android/build_aar.py --build-dir ../out/ --output ../out/libwebrtc.aar --arch "armeabi-v7a" "arm64-v8a" "x86_64" --extra-gn-args='is_debug=false' --verbose
 #tools_webrtc/android/release_aar.py --build-dir out/build_release_aar --verbose
